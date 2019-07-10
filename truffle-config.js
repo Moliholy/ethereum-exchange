0543af4bfc373d10c143c0e446d6bf9ae5b1da7d
Jose Molina Colmenero <molina@bity.com>
2019-07-10 13:37:15 +0200

    Add rinkeby config

--- truffle-config.js
+++ truffle-config.js
@@ -49,7 +49,9 @@ module.exports = {
         // },
 
         develop: {
-            port: 9545
+            host: 'localhost',
+            port: 9545,
+            network_id: '*',
         },
 
         // Another network with more advanced options...
@@ -63,15 +65,11 @@ module.exports = {
         // },
 
         // Useful for deploying to a public network.
-        // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: 4,
+        }
 
         // Useful for private networks
         // private: {
@@ -100,4 +98,4 @@ module.exports = {
             // }
         }
     }
-}
+};
