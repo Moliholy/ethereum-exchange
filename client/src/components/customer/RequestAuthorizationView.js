d6787634d76df460b83c34d936420c294048bdd7
Jose Molina Colmenero <molina@bity.com>
2019-07-20 01:28:59 +0200

    Add customer authorization view

--- client/src/components/customer/RequestAuthorizationView.js
+++ client/src/components/customer/RequestAuthorizationView.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Button, Header } from "semantic-ui-react";
+
+
+const requestAccess = async contract => {
+    try {
+        await contract.methods.requestAuthorization().send();
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 
 export default props => {
     return (
-        <h1>RequestAuthorizationView</h1>
+        <Header as='h2' icon textAlign='center'>
+            <Button style={{margin: 200}} size={'huge'} primary onClick={() => requestAccess(props.contract)}>
+                Request access
+            </Button>
+        </Header>
     );
 }
